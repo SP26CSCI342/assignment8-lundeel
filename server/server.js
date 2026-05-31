7e6c66ea2784ba3afed956427ee8bf3f79228f64
root <root@LAPTOP-BNEG0V7V.localdomain>
2026-05-30 18:04:48 -0700

    Fixed syntax error

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@
 
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
